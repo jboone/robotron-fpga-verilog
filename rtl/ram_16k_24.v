12ccd264bbe71a9174ae265f13ff757aeaa7ac8f
Jared Boone <jboone@earfeast.com>
2021-05-14 12:10:27 -0700

    Remove some dead Verilog code.

--- rtl/ram_16k_24.v
+++ rtl/ram_16k_24.v
@@ -12,82 +12,29 @@ module ram_16k_24(
 	output wire [23:0] data_out
 );
 
-wire wren = we[5] || we[4] || we[3] || we[2] || we[1] || we[0];
 
-`ifdef SIM
-
-reg [23:0] mem[0:16383];
-
-integer i;
-initial
-	for (i=0; i<16384; i++)
-		mem[i] = 0;
-
-assign data_out = wren ? 'bx : mem[addr];
-
-always @(posedge clk) begin
-	if(we[5] && wren) mem[addr][23:20] <= data_in[23:20];
-	if(we[4] && wren) mem[addr][19:16] <= data_in[19:16];
-	if(we[3] && wren) mem[addr][15:12] <= data_in[15:12];
-	if(we[2] && wren) mem[addr][11: 8] <= data_in[11: 8];
-	if(we[1] && wren) mem[addr][ 7: 4] <= data_in[ 7: 4];
-	if(we[0] && wren) mem[addr][ 3: 0] <= data_in[ 3: 0];
-end
-`endif
-
-// NOTE: Bad nomenclature, apparently POWEROFF=1 is power *on*!
-
-`ifdef ICE40
-SB_SPRAM256KA mem_0(
-	.DATAIN(data_in[15:0]),
-	.ADDRESS(addr),
-	.MASKWREN(we[3:0]),
-	.WREN(wren),
-	.CHIPSELECT(1'b1),
-	.CLOCK(clk),
-	.STANDBY(1'b0),
-	.SLEEP(1'b0),
-	.POWEROFF(1'b1),
-	.DATAOUT(data_out[15:0])
-);
-
-wire [15:0] data_out_ignored;
-
-SB_SPRAM256KA mem_1(
-	.DATAIN({ 8'h00, data_in[23:16] }),
-	.ADDRESS(addr),
-	.MASKWREN({ 2'b00, we[5:4] }),
-	.WREN(wren),
-	.CHIPSELECT(1'b1),
-	.CLOCK(clk),
-	.STANDBY(1'b0),
-	.SLEEP(1'b0),
-	.POWEROFF(1'b1),
-	.DATAOUT({ data_out_ignored, data_out[23:16] })
-);
-`endif
-
-`ifdef ECP5
-
-reg [23:0] mem[0:16383];
+reg [3:0] mem_0[0:16383];
+reg [3:0] mem_1[0:16383];
+reg [3:0] mem_2[0:16383];
+reg [3:0] mem_3[0:16383];
+reg [3:0] mem_4[0:16383];
+reg [3:0] mem_5[0:16383];
 
 wire [23:0] mem_i = data_in;
 
 reg [23:0] mem_o;
 
 always @(posedge clk) begin
-	mem_o <= mem[addr];
-
-	if(we[5]) mem[addr][23:20] <= mem_i[23:20];
-	if(we[4]) mem[addr][19:16] <= mem_i[19:16];
-	if(we[3]) mem[addr][15:12] <= mem_i[15:12];
-	if(we[2]) mem[addr][11: 8] <= mem_i[11: 8];
-	if(we[1]) mem[addr][ 7: 4] <= mem_i[ 7: 4];
-	if(we[0]) mem[addr][ 3: 0] <= mem_i[ 3: 0];
+	mem_o <= { mem_5[addr], mem_4[addr], mem_3[addr], mem_2[addr], mem_1[addr], mem_0[addr] };
+	
+	if(we[5]) mem_5[addr] <= mem_i[23:20];
+	if(we[4]) mem_4[addr] <= mem_i[19:16];
+	if(we[3]) mem_3[addr] <= mem_i[15:12];
+	if(we[2]) mem_2[addr] <= mem_i[11: 8];
+	if(we[1]) mem_1[addr] <= mem_i[ 7: 4];
+	if(we[0]) mem_0[addr] <= mem_i[ 3: 0];
 end
 
 assign data_out = mem_o;
-
-`endif
 
 endmodule
--- rtl/rom_64k_8.v
+++ rtl/rom_64k_8.v
@@ -16,27 +16,6 @@ wire bank = address_i[15];
 wire [13:0] sp_address = address_i[14:1];
 wire data_lane = address_i[0];
 
-`ifdef SIM
-`define SIM_OR_ECP5
-`endif
-
-`ifdef ECP5
-`define SIM_OR_ECP5
-`endif
-
-`ifdef SIM_OR_ECP5
-/*
-reg [7:0] mem[0:65535];
-initial
-	$readmemh(HEX_FILE, mem);
-
-reg [7:0] data_o;
-
-always @(posedge clock_i) begin
-	data_o <= mem[address_i];
-end
-*/
-
 reg [15:0] mem[0:32767];
 initial
 	$readmemh("build/rom.hex", mem);
@@ -48,55 +27,5 @@ always @(posedge clock_i) begin
 end
 
 assign data_o = data_lane ? mem_o[15:8] : mem_o[7:0];
-
-`endif
-
-`ifdef ICE40
-wire [15:0] sp_datain = { data_i, data_i };
-wire sp_clock = clock_i;
-wire sp_wren = write_enable_i;
-
-wire [3:0] sp_maskwren = data_lane ? 4'b1100 : 4'b0011;
-
-wire [1:0] sp_cs = { (bank == 1'b1), (bank == 1'b0) };
-
-wire [15:0] sp_dataout[0:1];
-wire [15:0] sp_dataout_bank = bank ? sp_dataout[1] : sp_dataout[0];
-
-// NOTE: Bad nomenclature, apparently POWEROFF=1'b1 is power *on*!
-
-(* keep="true" *)
-(* BEL="X25/Y0/spram_3" *)
-SB_SPRAM256KA mem_0(
-	.DATAIN(sp_datain),
-	.ADDRESS(sp_address),
-	.MASKWREN(sp_maskwren),
-	.WREN(sp_wren),
-	.CHIPSELECT(sp_cs[0]),
-	.CLOCK(sp_clock),
-	.STANDBY(1'b0),
-	.SLEEP(1'b0),
-	.POWEROFF(1'b1),
-	.DATAOUT(sp_dataout[0])
-);
-
-(* keep="true" *)
-(* BEL="X25/Y0/spram_4" *)
-SB_SPRAM256KA mem_1(
-	.DATAIN(sp_datain),
-	.ADDRESS(sp_address),
-	.MASKWREN(sp_maskwren),
-	.WREN(sp_wren),
-	.CHIPSELECT(sp_cs[1]),
-	.CLOCK(sp_clock),
-	.STANDBY(1'b0),
-	.SLEEP(1'b0),
-	.POWEROFF(1'b1),
-	.DATAOUT(sp_dataout[1])
-);
-
-assign data_o = data_lane ? sp_dataout_bank[15:8] : sp_dataout_bank[7:0];
-
-`endif
 
 endmodule
